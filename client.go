b7c0bf27eb67f1ef30dcb886da7896319a2711a5
eino <eino.oltedal@esa.int>
2016-03-08 01:15:10 +0100

    working on client..

--- client.go
+++ client.go
@@ -1,17 +1,40 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
+import (
+  "log";
+  "net";
+  "fmt";
+  "bufio";
+  "os"
+)
+
+func handleConnection(c net.Conn) {
+  buf := make([]byte, 4096)
+  
+  for {
+    n, err := c.Read(buf)
+    if err != nil || n == 0 {
+      c.Close()
+      break
+    }
+    n, err := c.Write(buf[0:n])
+    if err != nil {
+      c.Close()
+      break
+    }
+  }
+  log.Printf("Connection to %v closed.", C.RemoteAddr())
+}
 
 func main() {
+  msgchan := make(chan string)
+  go printMessages(msgchan)
 
-ln, err := net.Dial("tcp", "10.0.0.2:23")
-if err != nil {
-  //handle error
-}
-  for {
-    message, _ := bufio.NewReader(ln).ReadString('\n')
-    fmt.Print("Message Received:", string(message))
+  conn, err := net.Dial("tcp", "10.0.0.42:23")
+  if err != nil {
+    //handle error
+    log.Fatal(err)
+    os.Exit(1)
   }
+  go handleConnection(conn, msgchan)
 }
